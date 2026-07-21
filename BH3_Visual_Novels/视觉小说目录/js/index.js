6987320001be9dd967dc1e8577448db2bf34df3f
Escosis <1906317507@qq.com>
2026-07-21 23:45:04 +0800

    Move BH3-Visual-Novels Repository

--- BH3_Visual_Novels/视觉小说目录/js/index.js
+++
@@ -1,7 +0,0 @@
-  var mySwiper = new Swiper ('.swiper-container', {
-    speed: 2000,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })        
